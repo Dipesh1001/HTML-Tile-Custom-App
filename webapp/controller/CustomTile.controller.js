c4e90e189f056f5b9b50a7a30d46854400471af6
Ausno Brante <aubrtes@gmail.com>
2024-09-26 11:16:01 +0000

    Initial commit

---
+++ webapp/controller/CustomTile.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("html5.customtile.controller.CustomTile", {
+        onInit: function () {
+
+        }
+    });
+});
